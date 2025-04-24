f2e4c516175a368fbdffa544f9773a9d7cfc18ee
vijaysachan_sdn <vijaysachan@smartdatainc.net>
2025-04-24 10:49:47 +0530

    --

--- Package.swift
+++ Package.swift
@@ -11,6 +11,11 @@ let package = Package(
             name: "MyiOSLibrary",
             targets: ["MyiOSLibrary"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git",
+                   from: "11.12.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

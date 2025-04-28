f80d6991e4899e3557bb40af0e9bf548ad02ca07
vijaysachan_sdn <vijaysachan@smartdatainc.net>
2025-04-28 11:55:39 +0530

    logThreadType

---
+++ Sources/MyiOSLibrary/Code/Global/Global Functions.swift
@@ -0,0 +1,11 @@
+//
+//  Gloabl.swift
+//  MyiOSLibrary
+//
+//  Created by Vijay Sachan on 4/28/25.
+//
+import Foundation
+public func logThreadType(tag:String){
+    let threadName = Thread.isMainThread ? "Main Thread":"Background Thread"
+    print("logThreadType : [\(tag)] : \(threadName)")
+}

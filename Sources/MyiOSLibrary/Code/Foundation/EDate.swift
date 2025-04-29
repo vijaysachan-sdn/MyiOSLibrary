b60d5f815e474b7b4f1866207b47f3fb4d55fc88
vijaysachan_sdn <vijaysachan@smartdatainc.net>
2025-04-29 17:55:21 +0530

    Date

--- Sources/MyiOSLibrary/Code/Foundation/EDate.swift
+++ Sources/MyiOSLibrary/Code/Foundation/EDate.swift
@@ -63,28 +63,27 @@ extension Date:FWLoggerDelegate{
     /// - Date order
     /// - Time format (12-hour vs. 24-hour)
     /// - Time zone name/format
-    
     private func testDateTime(_ date:Date){
-//        mLog(msg: date.formatted(.dateTime))// 4/29/2025, 12:20 PM
-//        mLog(msg: date.formatted(.dateTime.year().month().day().weekday().hour().minute().second().timeZone().locale(.current)))// Tue, Apr 29, 2025 at 12:20:8 PM GMT+5:30
-//        // Year
-//        mLog(msg: date.formatted(.dateTime.year()))// 2025
-//        mLog(msg: date.formatted(.dateTime.year(.defaultDigits)))// 2025
-//        mLog(msg: date.formatted(.dateTime.year(.twoDigits)))// 25
-//        mLog(msg: date.formatted(.dateTime.year(.padded(6))))// 002025
-//        // Month
-//        mLog(msg: date.formatted(.dateTime.month())) // Apr
-//        mLog(msg: date.formatted(.dateTime.month(.abbreviated))) // Apr
-//        mLog(msg: date.formatted(.dateTime.month(.narrow))) // A
-//        mLog(msg: date.formatted(.dateTime.month(.defaultDigits))) // 4
-//        mLog(msg: date.formatted(.dateTime.month(.twoDigits))) // 04
-//        mLog(msg: date.formatted(.dateTime.month(.wide))) // April
-//        // Day
-//        mLog(msg: date.formatted(Date.FormatStyle().day(.defaultDigits))) // 29
-//        mLog(msg: date.formatted(Date.FormatStyle().day(.ordinalOfDayInMonth))) // 5 (It is the 5th Tuesday of the month)
-//        mLog(msg: date.formatted(Date.FormatStyle().day(.twoDigits))) // 29 // 09(If date was 9)
-//        mLog(msg: date.formatted(Date.FormatStyle().day())) // 29 // 9(If date was 9)
-//        // Weekday
+        mLog(msg: date.formatted(.dateTime))// 4/29/2025, 12:20 PM
+        mLog(msg: date.formatted(.dateTime.year().month().day().weekday().hour().minute().second().timeZone().locale(.current)))// Tue, Apr 29, 2025 at 12:20:8 PM GMT+5:30
+        // Year
+        mLog(msg: date.formatted(.dateTime.year()))// 2025
+        mLog(msg: date.formatted(.dateTime.year(.defaultDigits)))// 2025
+        mLog(msg: date.formatted(.dateTime.year(.twoDigits)))// 25
+        mLog(msg: date.formatted(.dateTime.year(.padded(6))))// 002025
+        // Month
+        mLog(msg: date.formatted(.dateTime.month())) // Apr
+        mLog(msg: date.formatted(.dateTime.month(.abbreviated))) // Apr
+        mLog(msg: date.formatted(.dateTime.month(.narrow))) // A
+        mLog(msg: date.formatted(.dateTime.month(.defaultDigits))) // 4
+        mLog(msg: date.formatted(.dateTime.month(.twoDigits))) // 04
+        mLog(msg: date.formatted(.dateTime.month(.wide))) // April
+        // Day
+        mLog(msg: date.formatted(Date.FormatStyle().day(.defaultDigits))) // 29
+        mLog(msg: date.formatted(Date.FormatStyle().day(.ordinalOfDayInMonth))) // 5 (It is the 5th Tuesday of the month)
+        mLog(msg: date.formatted(Date.FormatStyle().day(.twoDigits))) // 29 // 09(If date was 9)
+        mLog(msg: date.formatted(Date.FormatStyle().day())) // 29 // 9(If date was 9)
+        // Weekday
         mLog(msg: date.formatted(Date.FormatStyle().weekday())) // Tue
         mLog(msg: date.formatted(Date.FormatStyle().weekday(.abbreviated))) // Tue
         mLog(msg: date.formatted(Date.FormatStyle().weekday(.narrow))) // T
@@ -92,6 +91,54 @@ extension Date:FWLoggerDelegate{
         mLog(msg: date.formatted(Date.FormatStyle().weekday(.short))) // Tu
         mLog(msg: date.formatted(Date.FormatStyle().weekday(.twoDigits))) // 3
         mLog(msg: date.formatted(Date.FormatStyle().weekday(.wide))) // Tuesday
+        // Hour
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.defaultDigits(amPM: .abbreviated))))       // 12 PM
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.defaultDigits(amPM: .narrow)))) // 12 p
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.defaultDigits(amPM: .omitted)))) // 12
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.defaultDigits(amPM: .wide)))) // 12 PM
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.twoDigits(amPM: .abbreviated))))    // 12 PM
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.twoDigits(amPM: .narrow))))    // 12 p
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.twoDigits(amPM: .omitted))))    // 12
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.twoDigits(amPM: .wide))))    // 12 PM
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.conversationalDefaultDigits(amPM: .abbreviated)))) // 12 PM
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.conversationalDefaultDigits(amPM: .narrow)))) // 12 p
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.conversationalDefaultDigits(amPM: .omitted)))) // 12
+        mLog(msg: date.formatted(Date.FormatStyle().hour(.conversationalDefaultDigits(amPM: .wide)))) // 12 PM
+        // Minute
+        mLog(msg: date.formatted(Date.FormatStyle().minute(.defaultDigits))) // 20 // 5 (If Minute was 5)
+        mLog(msg: date.formatted(Date.FormatStyle().minute(.twoDigits))) //20 //  05 (If Minute was 5)
+        mLog(msg: date.formatted(Date.FormatStyle().minute())) //20 //  5 (If Minute was 5)
+        // Second
+        mLog(msg: date.formatted(Date.FormatStyle().second(.defaultDigits))) // 8
+        mLog(msg: date.formatted(Date.FormatStyle().second(.twoDigits))) // 08
+        mLog(msg: date.formatted(Date.FormatStyle().second())) // 8
+        // TimeZone
+        mLog(msg: date.formatted(.dateTime.timeZone()))                     // GMT+5:30
+        mLog(msg: date.formatted(.dateTime.timeZone(.specificName(.short))))// GMT+5:30
+        mLog(msg: date.formatted(.dateTime.timeZone(.specificName(.long)))) // India Standard Time
+        mLog(msg: date.formatted(.dateTime.timeZone(.genericLocation)))     // India Time
+        mLog(msg: date.formatted(.dateTime.timeZone(.genericName(.long))))  // India Standard Time
+        mLog(msg: date.formatted(.dateTime.timeZone(.genericName(.short)))) // India Time
+        mLog(msg: date.formatted(.dateTime.timeZone(.identifier(.long))))   // Asia/Kolkata
+        mLog(msg: date.formatted(.dateTime.timeZone(.identifier(.short))))  // inccu
+        mLog(msg: date.formatted(.dateTime.timeZone(.iso8601(.long))))      // +05:30
+        mLog(msg: date.formatted(.dateTime.timeZone(.localizedGMT(.short))))// GMT+5:30
+        mLog(msg: date.formatted(.dateTime.timeZone(.localizedGMT(.long)))) // GMT+05:30
+        mLog(msg: date.formatted(.dateTime.timeZone(.exemplarLocation)))    // Kolkata
+        // Locale
+        mLog(msg: date.formatted(.dateTime.locale(.current)))                       // 4/29/2025, 12:20 PM
+        // U.S. English (12-hour time, MM/DD/YYYY format)
+        mLog(msg: date.formatted(.dateTime.locale(Locale(identifier: "en_US"))))    // 4/29/2025, 12:20 PM
+        // British English (24-hour time, DD/MM/YYYY format)
+        mLog(msg: date.formatted(.dateTime.locale(Locale(identifier: "en_GB"))))    // 29/04/2025, 12:20
+        // German (24-hour time, DD.MM.YYYY format)
+        mLog(msg: date.formatted(.dateTime.locale(Locale(identifier: "de_DE"))))    // 29.4.2025, 12:20
+        // Japanese (Year first, with kanji characters)
+        mLog(msg: date.formatted(.dateTime.locale(Locale(identifier: "ja_JP"))))    // 2025/4/29 12:20
+        // Chinese (Simplified, China region)
+        mLog(msg: date.formatted(.dateTime.locale(Locale(identifier: "zh_CN"))))    // 2025/4/29 12:20
+        
+        
     }
     private func testRelative(_ date:Date){
         

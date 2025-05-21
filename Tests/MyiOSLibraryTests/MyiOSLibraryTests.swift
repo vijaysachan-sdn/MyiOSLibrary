28459a57fdbb5eaa0c8f7e7ede2f779bb3fb5c8c
vijaysachan_sdn <vijaysachan@smartdatainc.net>
2025-05-21 17:49:32 +0530

    YMCalendarView

--- Tests/MyiOSLibraryTests/MyiOSLibraryTests.swift
+++ Tests/MyiOSLibraryTests/MyiOSLibraryTests.swift
@@ -1,6 +1,27 @@
+import Foundation
 import Testing
 @testable import MyiOSLibrary
 
+
+
+
+
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    let nonSendable = NonSendable()
+    let safeClosure: @Sendable () -> Void = {
+        let localValue = 42  // Local values are safe
+        print("Safe operation: \(localValue)")
+    }
+    nonSendable.performAsyncTask(safeClosure)
+    
+    
+}
+class NonSendable{
+    func performAsyncTask(_ task: @Sendable @escaping() -> Void) {
+        Task {
+            task()  // This closure might execute on a different thread
+        }
+    }
 }
